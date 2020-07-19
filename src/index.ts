f13df94ea581d63a38c6cc85618c6e3e70a00dcc
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-07-19 23:16:18 +0100

    log

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,6 @@ const app = express();
  */
 app.use(express.json());
 app.use(morgan('dev'));
-app.get('/', (req, res) => console.log('Hello Node/Typescript starter!'));
+app.get('/', (req, res) => console.log('Url s!'));
 // eslint-disable-next-line import/prefer-default-export
 export { app };
