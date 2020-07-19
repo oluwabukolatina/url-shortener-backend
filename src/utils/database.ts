a8fad6b568e63888c7211d491c10677608ddb3dd
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-07-19 23:36:25 +0100

    log

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,6 +2,7 @@ import { connect } from 'mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
 const DB = String(process.env.APP_DB);
+console.log(DB)
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 async function connectToDb() {
   try {
