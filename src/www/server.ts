bb09435b5eb7e08dd67994c82581f200919b6d4c
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-07-19 23:35:27 +0100

    log

--- src/www/server.ts
+++ src/www/server.ts
@@ -10,6 +10,6 @@ if (!process.env.PORT) {
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  db.connectToDb().then(() => console.log('conoijhhhhhnected to db'))
-    .catch(() => console.log('sohj went wrong'));
+  db.connectToDb().then(() => console.log('codhjdhhdhdnoijhhhhhnected to db'))
+    .catch(() => console.log('sodhjddkjhj went wrong'));
 });
