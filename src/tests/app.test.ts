201b874f03958bb243d454791f54151c5ea8cbe2
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-07-22 01:02:10 +0100

    tests setup

---
+++ src/tests/app.test.ts
@@ -0,0 +1,14 @@
+import chai from 'chai';
+import 'chai/register-should';
+import chaiHttp from 'chai-http';
+import { app } from '../index';
+
+chai.use(chaiHttp);
+describe('Hello API Request', () => {
+  it('should return response on call', () => chai
+    .request(app)
+    .get('/')
+    .then((res) => {
+      chai.expect(res.text).to.eql('Hello');
+    }));
+});
